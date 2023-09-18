92a7b7eb488f52594c0dc961972798d9ac7c1229
necteo <cauresn06@gmail.com>
2023-09-18 14:43:05 +0900

    add

--- boj/11725_트리의_부모_찾기.py
+++ boj/11725_트리의_부모_찾기.py
@@ -1,22 +1,28 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-p = [i for i in range(n+1)]
-
-def find(u):
-    if p[u] != u:
-        p[u] = find(p[u])
-    return p[u]
-
-def union(u, v):
-    r1 = find(u)
-    r2 = find(v)
-    p[r1] = p[r2]
-
+arr = [[] for i in range(n+1)]
 for _ in range(n-1):
     a,b = map(int, input().split())
-    if b == 1 or find(b) == 1:
-        union(a,b)
-    else:
-        union(b,a)
+    arr[a].append(b)
+    arr[b].append(a)
+p = [i for i in range(n+1)]
 
-for i in range(2,n+1):
+def bfs(start):
+    visit = [0 for _ in range(n + 1)]
+    visit[start] = 1
+    queue = deque()
+    queue.append(start)
+    while queue:
+        v = queue.popleft()
+        for u in arr[v]:
+            if visit[u] == 0:
+                visit[u] = 1
+                p[u] = v
+                queue.append(u)
+
+bfs(1)
+for i in range(2, n+1):
     print(p[i])
