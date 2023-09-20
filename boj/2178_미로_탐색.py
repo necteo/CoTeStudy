c364cc02697cd0ce9e46b0186a5c34f484a2ec34
necteo <cauresn06@gmail.com>
2023-09-20 11:47:13 +0900

    add

--- boj/2178_미로_탐색.py
+++ boj/2178_미로_탐색.py
@@ -1,36 +1,37 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 n,m = map(int, input().split())
 arr = []
 for _ in range(n):
     arr.append(list(map(int, list(input().rstrip()))))
-visit= [[0 for _ in range(m)] for _ in range(n)]
-ans = [10000]
-def dfs(a,b,dep):
-    visit[a][b] = 1
-    if min(ans) < dep:
-        return
-    if a == n-1 and b == m-1:
-        if dep not in ans:
-            ans.append(dep)
-        return
-    if a > 0:
-        if visit[a-1][b] == 0 and arr[a-1][b] == 1:
-            dfs(a-1,b,dep+1)
-            visit[a-1][b] = 0
-    if b > 0:
-        if visit[a][b-1] == 0 and arr[a][b-1] == 1:
-            dfs(a,b-1,dep+1)
-            visit[a][b-1] = 0
-    if a < n-1:
-        if visit[a+1][b] == 0 and arr[a+1][b] == 1:
-            dfs(a+1,b,dep+1)
-            visit[a+1][b] = 0
-    if b < m-1:
-        if visit[a][b+1] == 0 and arr[a][b+1] == 1:
-            dfs(a,b+1,dep+1)
-            visit[a][b+1] = 0
-    dep -= 1
 
-dfs(0,0,1)
-print(min(ans))
+
+def bfs(a,b):
+    visit = [[0 for _ in range(m)] for _ in range(n)]
+    queue = deque()
+    queue.append([a,b])
+    cnt = 1
+    while queue:
+        for _ in range(len(queue)):
+            y,x = queue.popleft()
+            if visit[y][x] == 0:
+                visit[y][x] = 1
+                if y == n-1 and x == m-1:
+                    return cnt
+                if y > 0:
+                    if visit[y-1][x] == 0 and arr[y-1][x] == 1:
+                        queue.append([y-1,x])
+                if x > 0:
+                    if visit[y][x-1] == 0 and arr[y][x-1] == 1:
+                        queue.append([y,x-1])
+                if y < n-1:
+                    if visit[y+1][x] == 0 and arr[y+1][x] == 1:
+                        queue.append([y+1,x])
+                if x < m-1:
+                    if visit[y][x+1] == 0 and arr[y][x+1] == 1:
+                        queue.append([y,x+1])
+        cnt += 1
+
+
+print(bfs(0,0))
