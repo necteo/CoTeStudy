00d134fd7012e139da898e2c61eda0a6248746e7
necteo <cauresn06@gmail.com>
2023-09-30 23:13:48 +0900

    add 5585

---
+++ boj/5585_거스름돈.py
@@ -0,0 +1,21 @@
+n = 1000 - int(input())
+cnt = 0
+if n >= 500:
+    cnt += 1
+    n -= 500
+if n >= 100:
+    cnt += n // 100
+    n %= 100
+if n >= 50:
+    cnt += n // 50
+    n %= 50
+if n >= 10:
+    cnt += n // 10
+    n %= 10
+if n >= 5:
+    cnt += n // 5
+    n %= 5
+if n >= 1:
+    cnt += n // 1
+    n %= 1
+print(cnt)
