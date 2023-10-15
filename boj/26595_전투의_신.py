59a949fc277b86ee16e509f09310433ae60eb282
necteo <cauresn06@gmail.com>
2023-10-16 02:23:09 +0900

    add 26595

--- boj/26595_전투의_신.py
+++ boj/26595_전투의_신.py
@@ -1,10 +1,11 @@
 n = int(input())
 a, pa, b, pb = map(int, input().split())
 ans = [0, 0, 0]
-for i in range(n//pa, -1, -1):
-    j = n%i//pb if i != 0 else n//pb
-    s = a*i + b*j
-    if pa*i + pb*j <= n:
+for i in range(n+1):
+    x = i//pa if i != 0 else 0
+    y = (n-i)//pb if i != n else 0
+    s = a*x + b*y
+    if pa*x + pb*y <= n:
         if s > ans[0]:
-            ans = [s, i, j]
+            ans = [s, x, y]
 print(ans[1], ans[2])
