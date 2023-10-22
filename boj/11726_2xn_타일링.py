89427ea9210d7ab9d2243135a31e961d2c57fee9
necteo <cauresn06@gmail.com>
2023-10-23 00:43:12 +0900

    add 11726

---
+++ boj/11726_2xn_타일링.py
@@ -0,0 +1,11 @@
+dp = [1]
+n = int(input())
+for i in range(1,n+1):
+    # f(n) = f(n-1)+f(n-2)
+    if i > 0:
+        dp.append(dp[i-1])
+    if i > 1:
+        dp[i] += dp[i-2]
+# overflow 때문에 % 연산
+# 다른 언어의 경우 메모리 한계로 dp에 저장할 때 마다 % 연산이 필요
+print(dp[n] % 10007)
