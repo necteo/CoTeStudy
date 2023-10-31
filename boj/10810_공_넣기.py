650548988aab6405969f693361ef9c85aadbb5cc
necteo <cauresn06@gmail.com>
2023-10-31 23:00:48 +0900

    add 10810

---
+++ boj/10810_공_넣기.py
@@ -0,0 +1,7 @@
+n,m = map(int, input().split())
+basket = [0 for _ in range(n)]
+for _ in range(m):
+    i,j,k = map(int, input().split())
+    for b in range(i, j+1):
+        basket[b-1] = k
+print(*basket)
