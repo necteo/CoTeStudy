810211cb300739e4dd3355b4c5f2904dd224db1e
necteo <cauresn06@gmail.com>
2023-11-03 00:08:53 +0900

    add 10811

--- boj/10811_바구니_뒤집기.py
+++ boj/10811_바구니_뒤집기.py
@@ -2,6 +2,6 @@ n,m = map(int, input().split())
 basket = [i for i in range(1,n+1)]
 for _ in range(m):
     i,j = map(int, input().split())
-    for k in range(i-1, i+(j-i)//2):
-        basket[k], basket[j-1-k] = basket[j-1-k], basket[k]
+    for k in range((j-i)//2+1):
+        basket[i-1+k], basket[j-1-k] = basket[j-1-k], basket[i-1+k]
 print(*basket)
