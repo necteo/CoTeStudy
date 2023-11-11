578e1246935fbf0b5229dcb8e406ac4973f32507
necteo <cauresn06@gmail.com>
2023-11-11 21:26:30 +0900

    add 2776

---
+++ boj/2776_암기왕.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    N = input()
+    note1 = set(map(int, input().split()))
+    M = input()
+    note2 = list(map(int, input().split()))
+    for i in note2:
+        if i in note1:
+            print(1)
+        else:
+            print(0)
