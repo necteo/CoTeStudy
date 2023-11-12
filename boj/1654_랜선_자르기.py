7460cb2d5e64a7cb9d059ce339e9e973ea90cfa4
necteo <cauresn06@gmail.com>
2023-11-12 23:56:24 +0900

    fail 1654

---
+++ boj/1654_랜선_자르기.py
@@ -0,0 +1,18 @@
+K, N = map(int, input().split())
+arr = []
+for _ in range(K):
+    arr.append(int(input()))
+high = min(arr)
+low = 0
+mid = high
+while low+1 < high:
+    cnt = 0
+    for i in arr:
+        cnt += i//mid
+    if cnt < N:
+        high = mid
+        mid = (mid+low)//2
+    else:
+        low = mid
+        mid = (high+mid)//2
+print(mid)
