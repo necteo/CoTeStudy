bc5a43fa9d61af2d50afd89a1b5e6f489065523f
necteo <cauresn06@gmail.com>
2023-11-15 00:09:19 +0900

    add 2512

--- boj/2512_예산.py
+++ boj/2512_예산.py
@@ -2,13 +2,19 @@ N = int(input())
 money = list(map(int, input().split()))
 M = int(input())
 high = max(money)
-low = min(money)
+low = 0
 mid = high
-total = 0
-while total > M:
+while low+1 < high:
     total = 0
     for i in money:
         if i > mid:
             total += mid
         else:
             total += i
+    if total > M:
+        high = mid
+        mid = (mid+low)//2
+    else:
+        low = mid
+        mid = (mid+high)//2
+print(mid)
