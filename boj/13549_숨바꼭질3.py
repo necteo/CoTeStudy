1c628e698b0a058f650619ed470aa984bb0615c7
necteo <cauresn06@gmail.com>
2023-11-25 21:28:26 +0900

    add 13549

--- boj/13549_숨바꼭질3.py
+++ boj/13549_숨바꼭질3.py
@@ -1,12 +1,29 @@
+import heapq
+
+MAX = 100000
 N, K = map(int, input().split())
 t = 0
-while N != K:
-    if K >= N*2:
-        N *= 2
-    elif N < K:
-        N += 1
-        t += 1
-    elif N > K:
-        N -= 1
-        t += 1
-print(t)
+D = [MAX]*(MAX+1)
+
+def dijkstra(start, end):
+    q = []
+    heapq.heappush(q, (0, start))
+    D[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if D[now] < dist:
+            continue
+        D[now] = dist
+        if now == end:
+            break
+        if now < end:
+            if now != 0 and now*2 <= MAX:
+                heapq.heappush(q, (dist, now*2))
+            if now < MAX:
+                heapq.heappush(q, (dist+1, now+1))
+        if now > 0:
+            heapq.heappush(q, (dist+1, now-1))
+
+dijkstra(N, K)
+print(D[K])
