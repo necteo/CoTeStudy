fc3ce1b3ed504f70c4f0a36c18fed5f4ceb3c910
necteo <cauresn06@gmail.com>
2023-11-30 00:14:48 +0900

    add 10282

--- boj/10282_해킹.py
+++ boj/10282_해킹.py
@@ -1,5 +1,6 @@
 import heapq
 import sys
+input = sys.stdin.readline
 
 for _ in range(int(input())):
     n, d, c = map(int, input().split())
@@ -11,8 +12,23 @@ for _ in range(int(input())):
 
     q = []
     heapq.heappush(q, (0, c))
+    D[c] = 0
 
     while q:
         dist, now = heapq.heappop(q)
         if D[now] < dist:
             continue
+        for v, t in g[now]:
+            cost = D[now] + t
+            if cost < D[v]:
+                D[v] = cost
+                heapq.heappush(q, (cost, v))
+
+    cnt = 0
+    m = 0
+    for i in D:
+        if i < sys.maxsize:
+            cnt += 1
+            if m < i:
+                m = i
+    print(cnt, m)
