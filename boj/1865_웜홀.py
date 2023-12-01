9e45e9e0ec8f7c958d8bfdcce5a44c23ef9cc851
necteo <cauresn06@gmail.com>
2023-12-01 23:59:34 +0900

    add 1865

--- boj/1865_웜홀.py
+++ boj/1865_웜홀.py
@@ -1,25 +1,30 @@
 import sys
+input = sys.stdin.readline
 
 for _ in range(int(input())):
-    n, m, w = map(int, input().split())
-    g = [[] for _ in range(n+1)]
-    for i in range(m):
+    N, M, W = map(int, input().split())
+    g = [[] for _ in range(N+1)]
+    for i in range(M):
         s, e, t = map(int, input().split())
         g[s].append([e, t])
         g[e].append([s, t])
-    for i in range(w):
+    for i in range(W):
         s, e, t = map(int, input().split())
         g[s].append([e, -t])
-    ans = [sys.maxsize] * (n+1)
-    ans[1] = 0
-    for _ in range(n+1):
-        for u in range(1, n+1):
-            if ans[u] == sys.maxsize:
-                continue
-            for v, w in g[u]:
-                if ans[v] > ans[u] + w:
-                    ans[v] = ans[u] + w
-    if ans[1] < 0:
+    loop = []
+    for i in range(1, N+1):
+        ans = [sys.maxsize]*(N+1)
+        ans[i] = 0
+        for k in range(N+1):
+            for u in range(1, N+1):
+                if ans[u] == sys.maxsize:
+                    continue
+                for v, w in g[u]:
+                    if ans[v] > ans[u] + w:
+                        ans[v] = ans[u] + w
+            if k == N:
+                loop.append(True)
+    if True in loop:
         print("YES")
     else:
         print("NO")
