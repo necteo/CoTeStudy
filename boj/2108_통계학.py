f188bf5029a6a85021c63a47bf52d0b39d14239f
necteo <cauresn06@gmail.com>
2023-12-10 00:09:26 +0900

    add 2108

--- boj/2108_통계학.py
+++ boj/2108_통계학.py
@@ -7,7 +7,16 @@ for _ in range(N):
 arr.sort()
 print(round(sum(arr)/N))
 print(arr[N//2])
-cnt = []
-for i in range(N):
-
+idx = 0
+cnt = [[arr[idx], 1]]
+for i in range(1, N):
+    if arr[i] > arr[i-1]:
+        idx += 1
+        cnt.append([arr[i], 0])
+    cnt[idx][1] += 1
+cnt.sort(key=lambda x: x[1], reverse=True)
+if len(cnt) > 1 and cnt[0][1] == cnt[1][1]:
+    print(cnt[1][0])
+else:
+    print(cnt[0][0])
 print(arr[-1]-arr[0])
