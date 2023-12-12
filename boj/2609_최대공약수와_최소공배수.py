3b754811c891c875cc4928ea4bc15c0c02ef13a7
necteo <cauresn06@gmail.com>
2023-12-12 20:21:41 +0900

    add 2609

--- boj/2609_최대공약수와_최소공배수.py
+++ boj/2609_최대공약수와_최소공배수.py
@@ -1,5 +1,20 @@
 a, b = map(int, input().split())
 c, d = a, b
-x = max(a, b)
-while a != b:
-    
+x = 1
+y = 0
+while x != y:
+    y = x
+    for i in range(2, min(a, b)+1):
+        if a % i == 0 and b % i == 0:
+            a //= i
+            b //= i
+            x *= i
+            break
+print(x)
+a, b = c, d
+while c != d:
+    if c < d:
+        c += a
+    elif d < c:
+        d += b
+print(c)
