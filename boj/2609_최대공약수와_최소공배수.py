1a8afea5dfd9f7380a839147238becc8af6db5c1
necteo <cauresn06@gmail.com>
2023-12-12 20:31:25 +0900

    add 2609

--- boj/2609_최대공약수와_최소공배수.py
+++ boj/2609_최대공약수와_최소공배수.py
@@ -11,6 +11,14 @@ while x != y:
             x *= i
             break
 print(x)
+# 유클리드 호제법
+def gcd(r1, r2):
+    if r1 % r2 == 0:
+        return r2
+    return gcd(r2, r1%r2)
+
+print(gcd(max(c, d), min(c, d)))
+
 a, b = c, d
 while c != d:
     if c < d:
