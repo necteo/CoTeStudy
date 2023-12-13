e9992b8e72e91aada831fa6e77752c80c037389e
necteo <cauresn06@gmail.com>
2023-12-13 23:59:41 +0900

    add 4949

---
+++ boj/4949_균형잡힌_세상.py
@@ -0,0 +1,12 @@
+s = input()
+while s != ".":
+    check = []
+    for i in s:
+        if i == "(" or i == "[" or i == ")" or i == "]":
+            check.append(i)
+    ans = "yes"
+    while check:
+        c = check.pop()
+        if
+    print(ans)
+    s = input()
