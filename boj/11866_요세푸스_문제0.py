ed37033e78a083c0795505ad25bc0053af84ef86
necteo <cauresn06@gmail.com>
2023-12-25 00:14:35 +0900

    add 11866

--- boj/11866_요세푸스_문제0.py
+++ boj/11866_요세푸스_문제0.py
@@ -1,2 +1,11 @@
 N, K = map(int, input().split())
 arr = [i for i in range(1, N+1)]
+print("<", end="")
+length = len(arr)
+idx = 0
+while len(arr) > 1:
+    idx = (idx+K-1) % length
+    print(arr[idx], end=", ")
+    arr.pop(idx)
+    length -= 1
+print(arr[0], end=">")
