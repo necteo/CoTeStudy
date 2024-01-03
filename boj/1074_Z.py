d8bb4bf715a9f222b445da17da6e6fcad641a3d9
necteo <cauresn06@gmail.com>
2024-01-04 00:27:24 +0900

    add 1074

--- boj/1074_Z.py
+++ boj/1074_Z.py
@@ -1,9 +1,22 @@
 N, r, c = map(int, input().split())
 a = 0
 b = 2**(2*N)-1
+x, y = 0, 0
 l = 2**N-1
 
 while a != b:
-    if r < l//2+1:
-        if c < l//2+1:
-            b = b//4-1
+    if r < y+l//2+1:
+        if c > x+l//2:
+            a = a+4**(N-1)
+            x = x+l//2+1
+    else:
+        if c < x+l//2+1:
+            a = a+4**(N-1)*2
+        else:
+            a = a+4**(N-1)*3
+            x = x+l//2+1
+        y = y+l//2+1
+    b = a+4**(N-1)-1
+    N -= 1
+    l //= 2
+print(a)
