bac6517075f2b53d21d8dc8071729e97dfd65b26
necteo <cauresn06@gmail.com>
2024-01-03 23:59:28 +0900

    add 1074

--- boj/1074_Z.py
+++ boj/1074_Z.py
@@ -1,6 +1,9 @@
 N, r, c = map(int, input().split())
-t = 2**(2*N)
-a = 2**N-1
-b = 2**N-1
-x<a//2+1
-a//=2
+a = 0
+b = 2**(2*N)-1
+l = 2**N-1
+
+while a != b:
+    if r < l//2+1:
+        if c < l//2+1:
+            b = b//4-1
