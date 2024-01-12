6892ac6aaefa9c58581129fc1d92ce8e0f919b3f
necteo <cauresn06@gmail.com>
2024-01-12 23:59:42 +0900

    add 1697

---
+++ boj/1697_숨바꼭질.py
@@ -0,0 +1,27 @@
+import heapq
+N, K = map(int, input().split())
+MAX = 100000
+D = [MAX]*(MAX+1)
+
+def dij(start, end):
+    q = []
+    heapq.heappush(q, (0, start))
+    D[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if D[now] < dist:
+            continue
+        D[now] = dist
+        if now == end:
+            break
+        if now < end:
+            if now != 0 and now * 2 <= MAX:
+                heapq.heappush(q, (dist, now * 2))
+            if now < MAX:
+                heapq.heappush(q, (dist + 1, now + 1))
+        if now > 0:
+            heapq.heappush(q, (dist + 1, now - 1))
+
+dij(N, K)
+print(D[K])
