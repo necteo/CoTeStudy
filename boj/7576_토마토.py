5a8dacadfbe51fa880b0e089b7aebd794775a040
necteo <cauresn06@gmail.com>
2024-01-20 00:26:19 +0900

    add 7576

--- boj/7576_토마토.py
+++ boj/7576_토마토.py
@@ -2,32 +2,33 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 M, N = map(int, input().split())
+dy, dx = [0, 0, -1, 1], [-1, 1, 0, 0]
 tomato = []
 for _ in range(N):
     tomato.append(list(map(int, input().split())))
 
 def bfs():
-    visited = [[0 for _ in range(M)] for _ in range(N)]
     day = -1
     queue = deque()
     for i in range(N):
         for j in range(M):
             if tomato[i][j] == 1:
-                if visited[i][j] == 0:
-                    visited[i][j] = 1
-                    queue.append([i, j])
-    for _ in range(len(queue)):
-        while queue:
+                queue.append([i, j])
+    while queue:
+        for _ in range(len(queue)):
             y, x = queue.popleft()
-            if x > 0 and tomato[y][x-1] == 0:
-                queue.append([y, x-1])
-            elif x < M-1 and tomato[y][x+1] == 0:
-                queue.append([y, x+1])
-            elif y > 0 and tomato[y-1][x] == 0:
-                queue.append([y-1, x])
-            elif y < N-1 and tomato[y+1][x] == 0:
-                queue.append([y+1, x])
+            for i in range(4):
+                ny, nx = y+dy[i], x+dx[i]
+                if 0 <= ny < N and 0 <= nx < M:
+                    if tomato[ny][nx] == 0:
+                        tomato[ny][nx] = 1
+                        queue.append([ny, nx])
         day += 1
+    for i in range(N):
+        for j in range(M):
+            if tomato[i][j] == 0:
+                day = -1
+                break
     return day
 
 print(bfs())
