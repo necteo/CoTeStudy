2b0b04c129cf78339b037548fbe42dbc250dbd4d
necteo <cauresn06@gmail.com>
2024-01-26 00:22:27 +0900

    add 1149

--- boj/1149_RGB거리.py
+++ boj/1149_RGB거리.py
@@ -1,4 +1,14 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 home = []
 for _ in range(N):
     home.append(list(map(int, input().split())))
+ans = [home[0]]
+for i in range(1, N):
+    ans.append([sys.maxsize, sys.maxsize, sys.maxsize])
+    for j in range(3):
+        for k in range(3):
+            if j != k:
+                ans[i][k] = min(ans[i][k], ans[i-1][j]+home[i][k])
+print(min(ans[-1]))
