3ca89bad1f363db43a22f7d2cd929294e5ffff46
necteo <cauresn06@gmail.com>
2024-02-03 23:55:19 +0900

    add 1167

--- boj/1167_트리의_지름.py
+++ boj/1167_트리의_지름.py
@@ -7,20 +7,19 @@ for _ in range(V):
     for i in range(1, len(info)-1, 2):
         arr[info[0]].append([info[i], info[i+1]])
 
-def solve(v):
+def dfs(v):
     global d, ans
     visited[v] = 1
     for u, w in arr[v]:
         if visited[u] == 0:
-            visited[u] = 1
             d += w
-            solve(u)
+            print(d)
+            ans = max(d, ans)
+            dfs(u)
             d -= w
-    ans = max(d, ans)
 
 ans = 0
-for i in range(1, V+1):
-    d = 0
-    visited = [0 for _ in range(V+1)]
-    solve(i)
+d = 0
+visited = [0 for _ in range(V+1)]
+dfs(1)
 print(ans)
