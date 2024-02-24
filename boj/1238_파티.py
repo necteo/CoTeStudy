3070391143826279b3380bc9c54af1c22fbda3e2
necteo <cauresn06@gmail.com>
2024-02-24 23:59:12 +0900

    add 1238

--- boj/1238_파티.py
+++ boj/1238_파티.py
@@ -1,2 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
 N, M, X = map(int, input().split())
-arr = []
+arr = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    arr[a].append([b, c])
+
+def bfs(start, end):
+    visited = [0 for _ in range(N+1)]
+    visited[start] = 1
+    queue = deque()
+    queue.append(start)
+    s = 0
+    while queue:
+        v = queue.popleft()
+        for u, t in arr[v]:
+            if visited[u] == 0:
+                visited[u] = 1
+                s += t
+                if u == end:
+                    return s
+                queue.append(u)
+
+ans = 0
+for i in range(1, N+1):
+    if i != X:
+        total = bfs(i, X)+bfs(X, i)
+        print(total)
+        ans = max(ans, total)
+print(ans)
