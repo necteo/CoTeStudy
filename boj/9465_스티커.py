3c8bd8bac9c24c18bb96777fb7d13506d2047d17
necteo <cauresn06@gmail.com>
2024-07-29 23:55:52 +0900

    9465

--- boj/9465_스티커.py
+++ boj/9465_스티커.py
@@ -1,41 +1,32 @@
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
-    n = int(input)
-    sticker = []
+    n = int(input())
+    s = []
     for _ in range(2):
-        sticker.append(list(map(int, input().split())))
-    dp = [-1 for _ in range(n)]
+        s.append([0]+list(map(int, input().split())))
+    dp = [-1 for _ in range(n+1)]
+    dp[0] = 0
     sum = 0
 
-    
-
-    # if sticker[0][0]+max(sticker[1][1]+sticker[0][2], sticker[1][2]) < sticker[1][0]+max(sticker[0][1]+sticker[1][2], sticker[0][2]):
-    #     dp[0] = 1
-    #     if sticker[0][1]+sticker[1][2]< sticker[0][2]:
-    #         dp[2] = 0
-    #     else:
-    #         dp[1] = 0
-    # else:
-    #     dp[0] = 0
-    #     if sticker[1][1]+sticker[0][2] < sticker[1][2]:
-    #         dp[2] = 1
-    #     else:
-    #         dp[1] = 1
-
-    # i = 1
-    # while i < n-2:
-    #     if dp[i] == -1:
-    #         continue
-    #     if sticker[(dp[i]+1)%2][i+1]+sticker[dp[i]][i+2] < sticker[(dp[i]+1)%2][i+2]:
-    #         dp[i+1] = i+1
-    #         i+=1
-    #     else:
-    #         dp[i+2] = (dp[i]+1)%2
-    #         i+=2
-
-    # if dp[i] == -1:
-    #     if sticker[(dp[i]+1)%2][i+1]+sticker[dp[i]][i+2] < sticker[(dp[i]+1)%2][i+2]:
-    #         dp[i+1] = i+1
-    #         i+=1
-    #     else:
-    #         dp[i+2] = (dp[i]+1)%2
-    #         i+=2
+    for i in range(n):
+        a = sum+s[dp[i]][i]+s[(dp[i]+1)%2][i+1]
+        b = sum+s[dp[i]][i+1]
+        c = sum-s[dp[i-1]][i-1]+s[(dp[i]+1)%2][i]+s[dp[i]][i+1]
+        
+        if a >= b and a >= c:
+            sum += s[dp[i]][i]
+            dp[i+1] = (dp[i]+1)%2
+        elif b >= a and b >= c:
+            dp[i+1] = dp[i]
+            dp[i] = -1
+        elif c >= a and c >= b:
+            sum = sum-s[dp[i-1]][i-1]+s[(dp[i]+1)%2][i]
+            dp[i+1] = dp[i]
+            dp[i] = (dp[i]+1)%2
+            dp[i-1] = -1
+        print(dp, sum)
+        
+    sum += s[dp[-1]][-1]
+    print(sum)
