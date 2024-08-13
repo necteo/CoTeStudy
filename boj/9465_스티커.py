7e5d7ce2c06290a02f9b7d6ca64fb1e021ed7f44
necteo <cauresn06@gmail.com>
2024-08-13 17:47:58 +0900

    add 9465

--- boj/9465_스티커.py
+++ boj/9465_스티커.py
@@ -4,29 +4,26 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     n = int(input())
     s = []
+    dp = []
     for _ in range(2):
-        s.append([0]+list(map(int, input().split())))
-    dp = [-1 for _ in range(n+1)]
-    dp[0] = 0
-    sum = 0
+        s.append(list(map(int, input().split())))
+        dp.append([0 for _ in range(n)])
 
-    for i in range(n):
-        a = sum+s[dp[i]][i]+s[(dp[i]+1)%2][i+1]
-        b = sum+s[dp[i]][i+1]
-        c = sum-s[dp[i-1]][i-1]+s[(dp[i]+1)%2][i]+s[dp[i]][i+1]
-        
-        if a >= b and a >= c:
-            sum += s[dp[i]][i]
-            dp[i+1] = (dp[i]+1)%2
-        elif b >= a and b >= c:
-            dp[i+1] = dp[i]
-            dp[i] = -1
-        elif c >= a and c >= b:
-            sum = sum-s[dp[i-1]][i-1]+s[(dp[i]+1)%2][i]
-            dp[i+1] = dp[i]
-            dp[i] = (dp[i]+1)%2
-            dp[i-1] = -1
-        print(dp, sum)
-        
-    sum += s[dp[-1]][-1]
-    print(sum)
+    dp[0][0] = s[0][0]
+    dp[1][0] = s[1][0]
+
+    for i in range(1, n):
+        a = s[0][i]+dp[1][i-1]
+        b = s[0][i]+dp[1][i-2]  # if i==1: dp[-1] == 0
+        c = s[1][i]+dp[0][i-1]
+        d = s[1][i]+dp[0][i-2]
+
+        if a >= b:
+            dp[0][i] = a
+        else:
+            dp[0][i] = b
+        if c >= d:
+            dp[1][i] = c
+        else:
+            dp[1][i] = d
+    print(max(max(dp[0]), max(dp[1])))
